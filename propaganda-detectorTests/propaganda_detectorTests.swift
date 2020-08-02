a106397e015474d9beb5b829e0acd616b98c35c0
Kyle He <panyanhe99@gmail.com>
2020-08-01 22:36:24 -0700

    Initial Commit

---
+++ propaganda-detectorTests/propaganda_detectorTests.swift
@@ -0,0 +1,34 @@
+//
+//  propaganda_detectorTests.swift
+//  propaganda-detectorTests
+//
+//  Created by Kyle He on 8/1/20.
+//  Copyright © 2020 Kyle He. All rights reserved.
+//
+
+import XCTest
+@testable import propaganda_detector
+
+class propaganda_detectorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
